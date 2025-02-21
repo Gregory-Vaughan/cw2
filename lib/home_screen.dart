a5e8d554b0677ac68d53b0395537a9b0ef6ffdcb
Gregory-Vaughan <gpv520@gmail.com>
2025-02-21 18:33:59 -0500

    Made the recipes and the buttons to swap into the details screen.

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -2,20 +2,34 @@ import 'package:flutter/material.dart';
 import 'details_screen.dart';
 
 class HomeScreen extends StatelessWidget {
+  final List<String> recipes = [
+    'Taco',
+    'Sandwich',
+    'Cake',
+    'Cookie',
+    'Soup'
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Home Screen')),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => DetailsScreen()),
-            );
-          },
-          child: Text('Go to Details'),
-        ),
+      body: ListView(
+        padding: EdgeInsets.all(16.0),
+        children: recipes.map((recipe) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0),
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => DetailsScreen()),
+                );
+              },
+              child: Text(recipe),
+            ),
+          );
+        }).toList(),
       ),
     );
   }
